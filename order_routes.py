254c7ee055313c29694f2608364072901f5ff5a8
somesh-harshvardhan <somesh.harshvardhan@gmail.com>
2024-12-14 22:56:36 +0530

    order api

--- order_routes.py
+++ order_routes.py
@@ -45,3 +45,48 @@ async def place_an_order(order: OrderModel, Authorize: AuthJWT = Depends()):
         "order_status": new_order.order_status,
     }
     return jsonable_encoder(response)
+
+
+@order_router.get("/orders")
+async def list_all_orders(Authorize: AuthJWT = Depends()):
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Token"
+        )
+
+    current_user = Authorize.get_jwt_subject()
+
+    user = session.query(User).filter(User.username == current_user).first()
+    if user.is_staff:
+        orders = session.query(Order).all()
+
+        return jsonable_encoder(orders)
+
+    raise HTTPException(
+        status_code=status.HTTP_403_FORBIDDEN, detail="Access not allowed "
+    )
+
+
+@order_router.get("/order/{id}")
+async def get_order_by_id(id: int, Authorize: AuthJWT = Depends()):
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authorized"
+        )
+
+    user = Authorize.get_jwt_subject()
+
+    current_user = session.query(User).filter(user == User.username).first()
+
+    if current_user.is_staff:
+        order = session.query(Order).filter(id == Order.id).first()
+
+        return jsonable_encoder(order)
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="User not allowed to carry out request",
+    )
