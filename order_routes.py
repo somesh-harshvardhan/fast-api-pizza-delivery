d2eadaa732b3ac3c3a7e342711ed54878c072e7d
somesh-harshvardhan <somesh.harshvardhan@gmail.com>
2024-12-15 23:54:43 +0530

    update api

--- order_routes.py
+++ order_routes.py
@@ -90,3 +90,65 @@ async def get_order_by_id(id: int, Authorize: AuthJWT = Depends()):
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="User not allowed to carry out request",
     )
+
+
+@order_router.get("/user/orders")
+async def get_current_user_order(Authorize: AuthJWT = Depends()):
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authorized"
+        )
+    user = Authorize.get_jwt_subject()
+
+    current_user = session.query(User).filter(User.username == user).first()
+
+    orders = {"orders": current_user.orders}
+
+    return jsonable_encoder(orders)
+
+
+@order_router.get("/user/order/{order_id}")
+async def get_by_order_id(order_id: int, Authorize: AuthJWT = Depends()):
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authorized"
+        )
+
+    user = Authorize.get_jwt_subject()
+
+    current_user = session.query(User).filter(User.username == user).first()
+
+    order = list(filter(lambda order: order.id == order_id, current_user.orders))
+
+    return jsonable_encoder({"order": order})
+
+
+@order_router.put("/user/update/order/{order_id}")
+async def update_by_order_id(
+    order_id: int, order: OrderModel, Authorize: AuthJWT = Depends()
+):
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authorized"
+        )
+
+    user = Authorize.get_jwt_subject()
+    current_order = (
+        session.query(Order)
+        .join(User)
+        .filter(Order.id == order_id, User.username == user)
+        .first()
+    )
+
+    for key, value in order.dict(exclude_unset=True).items():
+        setattr(current_order, key, value)
+
+    session.commit()
+
+    return jsonable_encoder({"message": f"Updated order {order_id} successfully "})
